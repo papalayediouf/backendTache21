9bbea9c0bfbec09f913a792584984bca853640a5
Papa Abdoulaye Diouf <papalayediouf17@gmail.com>
2025-01-29 17:05:34 +0000

    image

--- config/baseDeDonnees.js
+++ config/baseDeDonnees.js
@@ -19,7 +19,7 @@ const connectDB = async () => {
 // Fonction pour ajouter un admin par défaut
 const creerAdminParDefaut = async () => {
     try {
-        const adminExistant = await Admin.findOne({ email: 'papalayediouf17@gmail.com' });
+        const adminExistant = await Admin.findOne({ email: 'soilihiambdouroihmane@gmail.com' });
         console.log('Admin existant:', adminExistant);  // Ajoute un log pour vérifier si l'admin existe déjà
 
         if (!adminExistant) {
