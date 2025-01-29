d426f72bb55dbfc126b07b2d3f6c3af0db752658
Papa Abdoulaye Diouf <papalayediouf17@gmail.com>
2025-01-29 14:57:07 +0000

    mot de passe

--- models/prestataireModele.js
+++ models/prestataireModele.js
@@ -10,10 +10,20 @@ const prestataireSchema = mongoose.Schema(
     telephone: { type: String, required: true },
     region: { type: String, required: true },
     departement: { type: String, required: true }, 
-    description: { type: String,  },
+    description: { type: String },
     role: {
       type: String,
       default: 'prestataire',
+    },
+    // Champ pour le code de réinitialisation
+    codeReset: {
+      type: String,
+      default: null,
+    },
+    // Champ pour la date d'expiration du code
+    codeResetExpire: {
+      type: Date,
+      default: null,
     },
   },
   { timestamps: true }
